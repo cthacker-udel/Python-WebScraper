07179284b6ee037057fe9b38cb9d69389fc88e96
cthacker-udel <cthacker@udel.edu>
2021-07-04 07:07:33 -0400

    Implemented beautiful soup to scrape the website for job information, role, name, logo link, time of posting, location, and link to apply, via automation
    Signed-off-by: cthacker-udel <cthacker@udel.edu>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from pprint import pprint
 from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup
+import regex as re
 
 def main():
     url = 'https://realpython.github.io/fake-jobs/'
@@ -15,8 +16,26 @@ def main():
 
     #pprint(job_listings)
 
-    all_jobs = job_listings.find_all("div",class_="card-content")
+    python_jobs = job_listings.find_all("h2",string=lambda text: 'python' in text.lower())
 
+    count = 1
+    for eachjob in python_jobs:
+        #pprint(eachjob.parent.parent.parent.parent)
+        role = eachjob.parent.find("h2",class_="title").text.strip()
+        company_name = eachjob.parent.find("h3",class_="subtitle").text.strip()
+        logo_link = eachjob.parent.parent.img['src'].strip()
+        time_of_posting = eachjob.parent.parent.parent.time.text.strip()
+        location = eachjob.parent.parent.parent.parent.find("p",class_="location").text.strip()
+        link_to_apply = eachjob.parent.parent.parent.parent.find("a",string=re.compile("[aA]pply"))['href'].strip()
+        print('\n----- Job {} -----\nRole : {}\nName : {}\nLogo Link : {}\nTime of Posting : {}\nLocation : {}\nLink To Apply : {}\n----------------------'.format(count,role,company_name,logo_link,time_of_posting,location,link_to_apply))
+        count += 1
+
+    #first_tag = python_jobs[0]
+
+    #pprint(first_tag.parent.parent.parent)
+
+    #all_jobs = job_listings.find_all("div",class_="card-content")
+    """
     for job_element in all_jobs:
         print('--------------\n')
         pprint(job_element)
@@ -32,6 +51,7 @@ def main():
         print(subtitle_content.text.strip())
         print(location_.text.strip())
         print(title_content.text.strip())
+    """
 
 
 
