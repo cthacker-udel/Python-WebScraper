57451eccd937550221e6e26f16ff12f2395e58fd
cthacker-udel <cthacker@udel.edu>
2021-07-04 06:15:50 -0400

    Added first webscraper for static web page
    Signed-off-by: cthacker-udel <cthacker@udel.edu>

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-# This is a sample Python script.
+import requests
+from pprint import pprint
+from requests.auth import HTTPBasicAuth
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    url = 'https://realpython.github.io/fake-jobs/'
+    page = requests.get(url)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint
-    print('hello')
-    # .
+    pprint(page.text)
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
