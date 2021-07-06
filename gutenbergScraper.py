3fdf60418012db9d35b37d48b9ef31b92090fb68
cthacker-udel <cthacker@udel.edu>
2021-07-06 07:38:04 -0400

    Added imports and base function for gutenbergScraper.py
    Signed-off-by: cthacker-udel <cthacker@udel.edu>

---
+++ gutenbergScraper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import selenium
+from webdriver_manager.opera import OperaDriverManager
+from webdriver_manager.firefox import DriverManager as FirefoxDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import urllib
+from selenium import webdriver
+from pprint import pprint
+
+def main():
+
+    print('')
+
+
+
+if __name__ == '__main__':
+    main()
